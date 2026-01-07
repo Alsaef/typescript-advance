c9d620b80829ba4bd6efe5711d1d1d47a3828b24
Alsaef <saef.ratul@gmail.com>
2026-01-07 16:38:11 +0600

    first commit

--- dist/moreTypes.js
+++ dist/moreTypes.js
@@ -5,5 +5,29 @@ let stringLength = response.length;
 console.log(stringLength);
 let userData = '{"name":"ratul"}';
 let userObject = JSON.parse(userData);
-userObject.name; // access name easyly 
+console.log(userObject.name); // access name easyly 
+let value = 'hello';
+value = [1, 2, 3, 4, 5, 6];
+value = 3.5;
+value = '99';
+if (value === 'string') {
+    value.toUpperCase();
+}
+// normal type handel
+try {
+}
+catch (error) {
+    console.log(error.message);
+}
+// type gard
+try {
+}
+catch (error) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+}
+// force fully type data
+let data = 'ratul';
+let name = data;
 //# sourceMappingURL=moreTypes.js.map
--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -14,4 +14,39 @@ let userData='{"name":"ratul"}'
 
 let userObject=JSON.parse(userData) as User
 
-userObject.name // access name easyly 
+console.log(userObject.name); // access name easyly 
+
+let value:unknown='hello';
+
+value=[1,2,3,4,5,6];
+value=3.5;
+value='99';
+
+if (value==='string') {
+    value.toUpperCase()
+}
+
+
+// normal type handel
+try {
+    
+} catch (error:any) {
+    console.log(error.message);
+}
+
+
+// type gard
+
+try {
+    
+} catch (error) {
+    if (error instanceof Error) {
+        
+        console.log(error.message); 
+    }
+}
+
+// force fully type data
+let data:unknown='ratul'
+
+let name:string=data as string
